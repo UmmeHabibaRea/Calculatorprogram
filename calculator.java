440e9cfbc4471f46240015854c68f1bcb22b4577
Umme Habiba Rea <rea.ru18@gmail.com>
2019-04-17 10:44:53 +0600

    called addition method in main

--- calculator.java
+++ calculator.java
@@ -1,5 +1,8 @@
 public class calculator{
 	public static void main(String[] args){
+		addition p = addition(4,5);
+		System.out.pritln(p);
+		
 		
 	}
 	public double SubtractionMethod(double x,double y){
