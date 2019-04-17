f2ad083fda23ecc748739a2046cd0a2de53be484
Prithu Rani Roy <prithuroy999@gmail.com>
2019-04-17 10:24:18 +0600

    subtraction method added in calculator class

--- calculator.java
+++ calculator.java
@@ -2,4 +2,9 @@ public class calculator{
 	public static void main(String[] args){
 		
 	}
+	public double SubtractionMethod(double x,double y){
+		double result;
+		result=x-y;
+		return result;
+	}
 }
